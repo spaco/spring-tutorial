15368fccc3028278400b85f04e9b77010d78f468
chentianzhu <chentianzhu@zeaho.com>
2020-06-23 16:54:57 +0800

    Add sleuth

--- sleuth/sleuth-order-service/src/main/java/com/example/sleuthorderservice/OrderController.java
+++ sleuth/sleuth-order-service/src/main/java/com/example/sleuthorderservice/OrderController.java
@@ -1,7 +1,9 @@
 package com.example.sleuthorderservice;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     @GetMapping("order")
-    public String order() {
+    public String order(@RequestHeader HttpHeaders headers) {
+        log.info("http header {}",headers.toString());
+
         log.info("order api is called !");
         return "order called success";
     }
--- sleuth/sleuth-user-service/src/main/java/com/example/sleuthuserservice/UserController.java
+++ sleuth/sleuth-user-service/src/main/java/com/example/sleuthuserservice/UserController.java
@@ -4,7 +4,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,7 +21,8 @@ public class UserController {
     }
 
     @GetMapping("user")
-    public String user() {
+    public String user(@RequestHeader HttpHeaders headers) {
+        log.info("http header {}",headers.toString());
         log.info("user api is called !");
         return restTemplate.getForEntity("http://sleuth-order-service/order", String.class).getBody();
     }
