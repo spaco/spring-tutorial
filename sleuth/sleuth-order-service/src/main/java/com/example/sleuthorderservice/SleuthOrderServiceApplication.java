cf6a02312140dc47a6d68e3f152233647d61aed8
chentianzhu <chentianzhu@zeaho.com>
2020-06-23 14:05:58 +0800

    Add sleuth

---
+++ sleuth/sleuth-order-service/src/main/java/com/example/sleuthorderservice/SleuthOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.sleuthorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleuthOrderServiceApplication.class, args);
+    }
+
+}
---
+++ sleuth/sleuth-order-service/src/test/java/com/example/sleuthorderservice/SleuthOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sleuthorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleuthOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
