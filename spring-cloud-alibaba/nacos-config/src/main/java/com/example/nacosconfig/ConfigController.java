19ceadabc8db4a6cc770d5773062d3f5920cf3c9
chentianzhu <chentianzhu@zeaho.com>
2020-08-13 10:09:09 +0800

    feature: add nacos

---
+++ spring-cloud-alibaba/nacos-config/src/main/java/com/example/nacosconfig/ConfigController.java
@@ -0,0 +1,23 @@
+package com.example.nacosconfig;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author spaco
+ */
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigController {
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}
